cb1e46a69d86bf4aa5f1ba8d54e388a2e20b1341
Antony Chan <antony.sigma@gmail.com>
2025-10-19 21:42:08 -0700

    Constexpr char

--- utils/pgm_string.hpp
+++ utils/pgm_string.hpp
@@ -9,13 +9,13 @@ namespace utils {
 struct PGMStringHelper : std::string_view {};
 }  // namespace utils
 
-#define PSTR2(s)                                                                          \
-    utils::PGMStringHelper {                                                              \
-        []() {                                                                            \
-            static const PROGMEM char message[]{s};                                       \
-            const size_t length = utils::Max(static_cast<int32_t>(std::size(s)) - 1, 0L); \
-            return std::string_view{&(message[0]), length};                               \
-        }()                                                                               \
+#define PSTR2(s)                                                                              \
+    utils::PGMStringHelper {                                                                  \
+        []() {                                                                                \
+            static constexpr PROGMEM char message[]{s};                                       \
+            constexpr size_t length = utils::Max(static_cast<int32_t>(std::size(s)) - 1, 0L); \
+            return std::string_view{&(message[0]), length};                                   \
+        }()                                                                                   \
     }
 
 // end define PSTR2
