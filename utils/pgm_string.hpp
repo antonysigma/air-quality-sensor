2ecb0ba95bfeff208d3726067611d6914c6cc760
Antony Chan <antony.sigma@gmail.com>
2026-08-18 13:34:37 -0700

    Remove null termination of PGM strings
    Atmega series MCUs uses Harvard architecture, in which `.rodata` section
    is absent. Previously, we have a C-macro `PSTR2()` that generates a
    C-string in flash memory at compile time.
    
    Since the string length is known at compile time, there's no need to
    store the null termination character in flash. For small strings, the
    compiler almost always unrolls the for-loop of
    `Serial::print("String")`.
    
    Implement `utils::PGMString` to remove the null termination character at
    compile-time.

--- utils/pgm_string.hpp
+++ utils/pgm_string.hpp
@@ -6,16 +6,22 @@
 
 namespace utils {
 
+template <std::size_t N>
+struct PGMString {
+    std::array<char, N - 1> buffer{};
+
+    consteval PGMString(const char (&s)[N]) { std::copy_n(s, N - 1, buffer.begin()); }
+};
+
 struct PGMStringHelper : std::string_view {};
 }  // namespace utils
 
-#define PSTR2(s)                                                                              \
-    utils::PGMStringHelper {                                                                  \
-        []() {                                                                                \
-            static constexpr PROGMEM char message[]{s};                                       \
-            constexpr size_t length = utils::Max(static_cast<int32_t>(std::size(s)) - 1, 0L); \
-            return std::string_view{&(message[0]), length};                                   \
-        }()                                                                                   \
+#define PSTR2(s)                                                                   \
+    utils::PGMStringHelper {                                                       \
+        []() {                                                                     \
+            static const PROGMEM utils::PGMString message{s};                      \
+            return std::string_view{message.buffer.data(), message.buffer.size()}; \
+        }()                                                                        \
     }
 
 // end define PSTR2
